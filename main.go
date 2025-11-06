4a51b6fc9f92e759cd3b64e138aa3515824638a7
Zakloni <edjovilellaca@ittepic.edu.mx>
2025-11-06 12:59:34 -0700

    Online users updated

--- main.go
+++ main.go
@@ -1,79 +1,88 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"sync"
-
-	"github.com/gorilla/websocket"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
-
 var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan []byte)
-var mu sync.Mutex
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
+var userCount = 0 // 👈 contador global
 
-func main() {
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-
-	http.HandleFunc("/ws", handleConnections)
-
-	go handleMessages()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	fmt.Println("🎨 Server running on port", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+type Message struct {
+    Type    string `json:"type"`
+    Content string `json:"content"`
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("Upgrade error:", err)
-		return
-	}
-	defer ws.Close()
+    upgrader.CheckOrigin = func(r *http.Request) bool { return true }
 
-	mu.Lock()
-	clients[ws] = true
-	mu.Unlock()
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Printf("Error upgrading connection: %v", err)
+        return
+    }
+    defer ws.Close()
 
-	log.Println("🟢 New client connected 🙋")
+    clients[ws] = true
+    userCount++ // 👈 incrementa al conectar
+    broadcastUserCount()
 
-	for {
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			log.Println("🔴 Client disconnected 👋:", err)
-			mu.Lock()
-			delete(clients, ws)
-			mu.Unlock()
-			break
-		}
-		broadcast <- msg
-	}
+    for {
+        var msg Message
+        err := ws.ReadJSON(&msg)
+        if err != nil {
+            delete(clients, ws)
+            userCount-- // 👈 decrementa al desconectar
+            broadcastUserCount()
+            break
+        }
+        broadcast <- msg
+    }
 }
 
 func handleMessages() {
-	for {
-		msg := <-broadcast
-		mu.Lock()
-		for client := range clients {
-			err := client.WriteMessage(websocket.TextMessage, msg)
-			if err != nil {
-				log.Println("Error sending:", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
-		mu.Unlock()
-	}
+    for {
+        msg := <-broadcast
+        for client := range clients {
+            err := client.WriteJSON(msg)
+            if err != nil {
+                log.Printf("Error sending message: %v", err)
+                client.Close()
+                delete(clients, client)
+                userCount--
+                broadcastUserCount()
+            }
+        }
+    }
+}
+
+func broadcastUserCount() {
+    msg := Message{
+        Type:    "userCount",
+        Content: fmt.Sprintf("%d users online", userCount),
+    }
+    for client := range clients {
+        client.WriteJSON(msg)
+    }
+}
+
+func main() {
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+
+    fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fs)
+    http.HandleFunc("/ws", handleConnections)
+
+    go handleMessages()
+
+    log.Printf("🎨 Server running on http://localhost:%s", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
